c922930bde9185f718bb31d688133c4a0cc7058d
Noir00 <vitaliykorolyk95@gmail.com>
2020-01-05 12:58:30 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+var number = 5;
+var string = "hello!";
+var sym = Symbol();
+null;
+undefined;
+var obj = {};
+
+console.log(4 / 0);
+console.log('sting' / 9);
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+//alert("Hello World!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = prompt("Are you here?", "Yes");
+// console.log(typeof (answer));
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
