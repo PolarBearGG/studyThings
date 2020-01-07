afb36efc6bd0e9c2148ed55faddb6b602d9ac371
Noir00 <vitaliykorolyk95@gmail.com>
2020-01-07 20:49:14 +0200

    add comments to func in task3

--- Project3/script.js
+++ Project3/script.js
@@ -37,14 +37,14 @@ function chooseExpenses() {
 
 chooseExpenses();
 
-function detectDayBudget() {
+function detectDayBudget() { // Расчет дневного бюджета
     appData.moneyPerDay = (appData.budget / 30).toFixed();
     alert("Бюджет на день:" + appData.moneyPerDay);
     console.log(appData.moneyPerDay);
 }
 detectDayBudget();
 
-function detectLevel() {
+function detectLevel() { // Расчет уровня достатка
     if (appData.moneyPerDay < 100) {
         console.log("Низкий уровень достатка");
     } else if (appData.moneyPerDay > 100 & appData.moneyPerDay < 2000) {
@@ -71,13 +71,13 @@ function checkSavings() {
 }
 checkSavings();
 
-function chooseOptExpenses() {
+function chooseOptExpenses() { // Функция для определения необязательных расходов
     for (let i = 0; i < 3; i++) {
         let noneRequired = prompt("Введите не обязательную статью расходов в этом месяце", ""),
             noneRequiredBudget = +prompt("Во сколько обойдется?", "");
         if ((typeof (noneRequired)) === 'string' && (typeof (noneRequired)) != null &&
             (typeof (noneRequiredBudget)) != null && noneRequired != '' && noneRequiredBudget != '' && noneRequired.length < 50) {
-            appData.optionalExpenses[i+1] = noneRequiredBudget;
+            appData.optionalExpenses[i + 1] = noneRequiredBudget;
         } else {
             i--;
         }
