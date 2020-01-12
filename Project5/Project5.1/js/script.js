f52d9e9cb214ec3bd62e56b6bc828135ad7c09e5
Noir00 <vitaliykorolyk95@gmail.com>
2020-01-12 19:10:49 +0200

    task 5.1

---
+++ Project5/Project5.1/js/script.js
@@ -0,0 +1,36 @@
+document.body.style.backgroundImage = 'url("../Project5.1/img/apple_true.jpg")'; //зміна бекграунду
+
+document.getElementById('title').textContent = 'Мы продаем только подлинную технику Apple'; //зміна заголовку на потрібний
+
+let prom = document.getElementById('prompt'),
+    div = document.createElement('div');
+
+    div.classList.add("black");
+    prom.appendChild(div);
+
+ let qw = prompt("Какое у вас  отношение к технике apple?", ""); //задати питання і відобразити відповідь на нього в блоці
+
+     div.textContent = qw;      //створення блоку в блоці і внесення туди тексту
+    
+let col = document.querySelectorAll('.column'),
+    adv = document.getElementsByClassName('adv')[0];
+    adv.remove(); //видалення реклами
+
+ 
+
+ let menuItemTri = document.getElementsByClassName('menu-item')[1], //сортую і додаю 5 пункт
+     menuItemFour = document.getElementsByClassName('menu-item')[3],
+    menu =  document.getElementsByClassName('menu')[0],
+    menuItemFive = document.createElement('li');
+    
+    menu.appendChild(menuItemTri);
+    menu.appendChild(menuItemFour);
+
+    menuItemFive.classList.add("menu-item");
+    menu.appendChild(menuItemFive).textContent = 'Пятий пункт';
+
+
+ 
+
+
+
