fbf6434e6b6ee18814b4fa4e50f15e3e62d487db
Noir00 <vitaliykorolyk95@gmail.com>
2020-01-12 20:29:41 +0200

    task6

---
+++ Project6/js/main.js
@@ -0,0 +1,22 @@
+let start = document.getElementById('start')[0],
+budgetValue = document.getElementsByClassName('budget-value')[0],
+daybudgetValue = document.getElementsByClassName('daybudget-value')[0],
+levelValue = document.getElementsByClassName('level-value')[0],
+expensesValue = document.getElementsByClassName('expenses-value')[0],
+optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+incomeValue = document.getElementsByClassName('income-value')[0],
+monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+expensesItem = document.getElementsByClassName('expenses-item'),
+btnStart1 = document.getElementsByTagName('button')[0],
+btnStart2 = document.getElementsByTagName('button')[1],
+btnCount = document.getElementsByTagName('button')[2],
+optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+chooseIncome = document.querySelector('.choose-income'),
+savings = document.querySelector('#savings'), //# - це id, . - це class
+summa = document.querySelector('#sum'),
+percent = document.querySelector('#percent'),
+yearValue = document.querySelector('.year-value'),
+monthValue = document.querySelector('.month-value'),
+dayValue = document.querySelector('.day-value');
+   
