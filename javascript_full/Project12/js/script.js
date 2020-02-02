3c4c52f9c23eb94f7e866fc6749c4e97e7995653
Noir00 <vitaliykorolyk95@gmail.com>
2020-02-02 14:07:53 +0200

    task14 - ajax, post form request

--- javascript_full/Project12/js/script.js
+++ javascript_full/Project12/js/script.js
@@ -104,13 +104,13 @@ window.addEventListener('DOMContentLoaded', () => {
         };
 
     function all(test) {
-        test.addEventListener('click',  () => {
+        test.addEventListener('click', () => {
             overlay.style.display = 'block';
             this.classList.add('more-splash');
             document.body.style.overflow = 'hidden';
         });
 
-        close.addEventListener('click',  () => {
+        close.addEventListener('click', () => {
             overlay.style.display = 'none';
             test.classList.remove('more-splash');
             document.body.style.overflow = '';
@@ -127,4 +127,82 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
+
+
+
+
+    // Form 1
+
+    let message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        contactForm = document.getElementById('form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    function getData(data) {
+        data.addEventListener('submit', function (event) {
+            event.preventDefault();
+            data.appendChild(statusMessage);
+            
+            let formData = new FormData(data);
+
+            function postData() {
+                return new Promise(function (resolve,reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        
+                   
+        
+                    let obj = {};
+                    formData.forEach(function (value, key) {
+                        obj[key] = value;
+                    });
+                    let json = JSON.stringify(obj);
+        
+                    request.send(json);
+        
+                    request.addEventListener("readystatechange", function () {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4 && request.status === 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+        
+        
+                    });
+                });
+               
+            }
+            
+
+
+function clearInput() {
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = "";
+            }}
+
+
+            postData(formData)
+            .then (() => statusMessage.innerHTML = message.loading)
+            .then (() => statusMessage.innerHTML = message.success)
+            .catch (() => statusMessage.innerHTML = message.failure)
+            .then (clearInput);
+
+
+
+
+        });
+    }
+    getData(form);
+    getData(contactForm);
 });
